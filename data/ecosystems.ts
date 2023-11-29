bc62dde9b3dc7613e99d876102795956825e5896
Thomas Gram <thomas@codeshark.net>
2023-11-29 10:13:01 +0100

    Rename sustainfest endpoint to openclimateaction, don't include github workflows for now.

--- data/ecosystems.ts
+++ data/ecosystems.ts
@@ -1,7 +1,7 @@
 import type { Project } from "@/types/apiTypes";
 
 export async function GetAllProjects() {
-  const response = await fetch("https://ost.ecosyste.ms/api/v1/issues/sustainfest");
+  const response = await fetch("https://ost.ecosyste.ms/api/v1/issues/openclimateaction");
   const data = (await response.json()) as Project[];
   return data;
 }
