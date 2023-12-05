5cef6dabbe4c5a0d8e5eca050428814e5408052a
Thomas Gram <thomas@codeshark.net>
2023-12-05 12:11:13 +0100

    Add initial about section.

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,71 +1,20 @@
+import config from "../app/config.mts";
 import { SectionTitle } from "./SectionTitle";
 
 export const AboutSection = () => {
   return (
     <div>
       <SectionTitle className="mb-2" text="About" />
-      <p>Coming soon...</p>
-      {/* <h1 className="hidden text-lg font-bold">verto.sh</h1>
+      <h1 className="hidden text-lg font-bold">{config.meta.title}</h1>
       <p className="text-silver-500">
-        <strong>verto.sh</strong> simplifies your entry into the open-source
-        world by curating accessible projects. Ideal for beginners making their
-        first contribution, founders building teams, and maintainers seeking new
-        contributors.
+        <strong>{config.meta.title}</strong> is a unique platform dedicated to harnessing the power
+        of open source collaboration in tackling environmental challenges. We provide a curated
+        directory of impactful projects focused on climate change, energy, biodiversity, and natural
+        resource management. Whether you're a seasoned developer or a newcomer eager to contribute,
+        our platform connects you with meaningful opportunities to apply your skills towards
+        creating a sustainable future. Join our community of forward-thinking developers and make
+        your first impactful contribution today!
       </p>
-      <h2 className="mt-2">Your Gateway to Open-Source Collaboration!</h2> */}
-      {/* <div className="pt-6 text-sm">
-        <Link
-          className="flex flex-row items-center"
-          target="_blank"
-          href="https://www.lucavall.in"
-        >
-          <FontAwesomeIcon icon={faHeart} />
-          <span className="ml-2">
-            A{" "}
-            <span
-              className="inline font-bold text-primary transition-all hover:underline"
-              title="Visit GitHub profile"
-            >
-              @lucavallin
-            </span>{" "}
-            & contributors initiative
-          </span>
-        </Link>
-        <Link
-          className="flex flex-row items-center text-base"
-          title="Chiara Dametto on LinkedIn"
-          aria-label=" UI/UX Design by Chiara Dametto"
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://www.linkedin.com/in/chiara-dametto"
-        >
-          <FontAwesomeIcon icon={faPaintRoller} />
-          <span className="ml-2">
-            UI/UX Design by{" "}
-            <span className="inline font-bold text-primary transition-all hover:underline">
-              Chiara Dametto
-            </span>{" "}
-          </span>
-        </Link>
-        <Link
-          className="flex cursor-pointer flex-row items-center text-base text-silver-500"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="View Privacy Policy"
-          aria-label="Privacy & Cookie Policy"
-          href="https://www.privacyboard.co/company/lucavallin"
-        >
-          <FontAwesomeIcon icon={faCircleInfo} />
-          <span className="ml-2">
-            <span
-              className="inline text-base text-gray-900 transition-all hover:underline"
-              title="View Privacy Policy"
-            >
-              Privacy & Cookie Policy
-            </span>
-          </span>
-        </Link>
-      </div> */}
     </div>
   );
 };
