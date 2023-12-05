800611bbc987527af6f1736339c01703361c4c8d
Thomas Gram <thomas@codeshark.net>
2023-12-05 12:09:41 +0100

    Get site name from config. Display active categories in the header also.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-
 import { Tag } from "@/types/types";
+import Link from "next/link";
 import { useParams } from "next/navigation";
+import config from "../app/config.mts";
 import { useAppData } from "../hooks/useAppData";
 
 export const Header = () => {
   const { tag: activeTagId, language: activeLanguageId } = useParams();
-  const { languages, tags } = useAppData();
-  const activeTagOrLanguage = [...languages, ...tags].find(
+  const { languages, categories, tags } = useAppData();
+  const activeFilter = [...languages, ...categories, ...tags].find(
     (t: Tag) => t.id === activeTagId || t.id === activeLanguageId
   );
 
@@ -15,12 +15,12 @@ export const Header = () => {
     <header className="container mx-auto my-5 flex flex-col items-center bg-black-400 p-6 md:my-10 md:flex-row md:justify-between lg:max-w-6xl">
       <nav className="flex flex-wrap items-center md:justify-center">
         <Link href="/" title="Home" aria-label="Home">
-          Open Sustainable Technology
+          {config.meta.title}
           {/* <Image src="/verto.png" alt="verto.sh" className="h-12" width={320} height={52} /> */}
         </Link>
-        {activeTagOrLanguage ? (
-          <span className="ml-8 cursor-pointer pt-4 text-4xl font-semibold text-gray">
-            {activeTagOrLanguage?.display}
+        {activeFilter ? (
+          <span className="ml-8 cursor-pointer pt-4 text-4xl font-semibold text-silver-500">
+            {activeFilter?.display}
           </span>
         ) : null}
       </nav>
